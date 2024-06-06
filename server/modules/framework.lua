40dc2349266492d7b35ebd6d8983db7da9780d96
abdel1touimi <abdel1.touimi@gmail.com>
2024-06-06 20:32:56 +0100

    add qbx support + minor types fix

--- server/modules/framework.lua
+++ server/modules/framework.lua
@@ -12,6 +12,10 @@ local function createFrameworkAdapter(framework)
         function adapter.getCharacterIdentifier(source)
             return exports.qbx_core:GetPlayer(source)?.PlayerData?.citizenid
         end
+    elseif framework == 'qbx' then
+        function adapter.getCharacterIdentifier(source)
+            return exports.qbx_core:GetPlayer(source)?.PlayerData?.citizenid
+        end
     elseif framework == 'esx' then
         local coreObject = exports['es_extended']:getSharedObject()
         function adapter.getCharacterIdentifier(source)
